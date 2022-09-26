7cf3ec489ecf04b439ede409e01d0361a2a6effe
Phariq <pharisnjuguna22@gmail.com>
2022-09-26 10:31:05 +0300

    First commit of javascript DOM manipulation

---
+++ main.js
@@ -0,0 +1,49 @@
+
+let titleDiv = document.getElementById('title');
+console.log('before: ',
+titleDiv.innerText);
+
+let message = 'Box game'
+titleDiv.innerText = message;
+console.log('after: ',
+titleDiv.innerText);
+
+titleDiv.innerHTML =`<p>${message}</p>`
+
+titleDiv.style.backgroundColor = "Orange";
+
+let blueDiv = document.getElementById('blue');
+let redDiv = document.getElementById('red');
+let greenDiv = document.getElementById('green');
+
+
+const squares = document.querySelectorAll('.colorBtn');
+console.log(squares);
+
+//forEach loop combining onclick , gets values and prints on squares
+
+const timesClicked = { 'blue': 0, 'red':0, 'green':0
+}
+squares.forEach(square =>{
+    square.onclick = () => {
+timesClicked[square.value] += 1
+square.innerText = timesClicked[square.value];
+         
+    }
+});
+
+// Clearing the game
+//first we make values of all onclick instances to 0 to return default 
+
+const clearScores =() => {
+    timesClicked.blue = 0
+    timesClicked.red = 0
+    timesClicked.green = 0
+    squares.forEach(square =>{
+        square.innerText = 0 
+    })
+}
+//get the id of the clear game button
+const clearScoresBtn = document.getElementById('clear game')
+//what it does when clicked. as a function it clears scores as was specified earlier
+clearScoresBtn.onclick = () => clearScores()
